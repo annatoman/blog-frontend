44ddbfd83c27fd9825a19c7deb8891ca827bc653
Anna <annamtoman@gmail.com>
2023-04-14 11:33:38 -0500

    imports modal into content and sets test modal to true

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from "axios";
 import { PostsNew } from './PostsNew';
 import { PostsIndex } from './PostsIndex';
+import { Modal } from './Modal';
 
 export function Content() {
   const [posts, setPosts] = useState([]);
@@ -19,6 +20,9 @@ export function Content() {
     <div>
       <PostsNew />
       <PostsIndex posts={posts} />
+      <Modal show={true}>
+        <p>TEST</p>
+      </Modal>
     </div>
   );
 }
