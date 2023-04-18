43d5c9a1ed51c0f24a0dbbf53fe96bc0581174da
Anna <annamtoman@gmail.com>
2023-04-18 14:11:12 -0500

    adds update post function

--- src/Content.jsx
+++ src/Content.jsx
@@ -35,6 +35,21 @@ export function Content() {
     });
   };
 
+  const handleUpdatePost = (id, params) => {
+    axios.patch("http://localhost:3000/posts/" + id + ".json", params).then((response) => {
+      setPosts(
+        posts.map((post) => {
+          if (post.id === response.data.id) {
+            return response.data;
+          } else {
+            return post;
+          }
+        })
+      );
+      handleClose();
+    })
+  }
+
   useEffect(handleIndexPosts, []);
 
   return (
@@ -45,7 +60,7 @@ export function Content() {
       <PostsNew onCreatePost={handleCreatePost}/>
       <PostsIndex posts={posts} onShowPost={handleShowPost}/>
       <Modal show={isPostsShowVisible} onClose={handleClose}>
-      <PostsShow post={currentPost}/>
+      <PostsShow post={currentPost} onUpdatePost={handleUpdatePost}/>
       </Modal>
     </div>
   );
--- src/PostsShow.jsx
+++ src/PostsShow.jsx
@@ -1,14 +1,10 @@
-import axios from "axios";
-
 export function PostsShow(props) {
     
     const handleSubmit = (event) => {
         event.preventDefault();
         const params = new FormData(event.target);
-        axios.patch("http://localhost:3000/posts/" + props.post.id + ".json", params).then((response) => {
-            console.log("handleSubmit");
-            event.target.reset();
-        });
+        props.onUpdatePost(props.post.id, params);
+        event.target.reset();
      };
 
     return (
